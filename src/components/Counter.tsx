5e3adeaeecc9726f4d24181d756d261eccc4b32a
rbaruah <31023635+Rakesh6720@users.noreply.github.com>
2023-11-22 20:59:11 +0000

    Update Counter.tsx and counterSlice.ts

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../state/counter/counterSlice";
+import {
+  decrement,
+  decrementAsync,
+  increment,
+  incrementAsync,
+} from "../state/counter/counterSlice";
 import { AppDispatch, RootState } from "../state/store";
 
 export default function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);
+  const isLoading = useSelector((state: RootState) => state.counter.isLoading);
   const dispatch = useDispatch<AppDispatch>();
   return (
     <>
       <h2>Counter</h2>
-      <p>{count}</p>
+      {isLoading ? <p>---</p> : <p>{count}</p>}
       <div
         style={{
           width: 300,
@@ -19,6 +25,30 @@ export default function Counter() {
       >
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+      <div
+        style={{
+          marginTop: 25,
+          width: 300,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <button onClick={() => dispatch(incrementAsync(10))}>
+          Increment +10 Async
+        </button>
+      </div>
+      <div
+        style={{
+          marginTop: 25,
+          width: 300,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <button onClick={() => dispatch(decrementAsync(10))}>
+          Decrement -10 Async
+        </button>
       </div>
     </>
   );
--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -1,11 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
+  isLoading: boolean;
+  isError: boolean;
 }
 
 const initialState: CounterState = {
   value: 0,
+  isLoading: false,
+  isError: false,
 };
 
 const counterSlice = createSlice({
@@ -19,7 +23,57 @@ const counterSlice = createSlice({
       state.value -= 1;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(incrementAsync.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+    }),
+      builder.addCase(
+        incrementAsync.fulfilled,
+        (state, action: PayloadAction<number>) => {
+          state.value += action.payload;
+          state.isLoading = false;
+          state.isError = false;
+        },
+      ),
+      builder.addCase(incrementAsync.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+      }),
+      builder.addCase(decrementAsync.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+      }),
+      builder.addCase(
+        decrementAsync.fulfilled,
+        (state, action: PayloadAction<number>) => {
+          state.isLoading = false;
+          state.isError = false;
+          state.value -= action.payload;
+        },
+      ),
+      builder.addCase(decrementAsync.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+      });
+  },
 });
+
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  },
+);
+
+export const decrementAsync = createAsyncThunk(
+  "counter/decrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  },
+);
 
 export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
